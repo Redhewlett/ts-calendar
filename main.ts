ffd651df6aba06174f100ecec23f7d50a4b6a91f
Redhewlett <edmondteddy@hotmail.fr>
2025-03-17 10:43:37 +0100

    adding types, using locales, and sending the strings to make the arrays

--- main.ts
+++ main.ts
@@ -1,43 +1,76 @@
+import { formatOptions, locales } from "./types.ts";
 
-export function generateCalendar(year:number, month:number, locale:string = 'en-US') {
+const defaultFormatOptions: formatOptions = {
+  weekday: "long",
+  month: "long",
+  day: "2-digit",
+  year: "numeric",
+};
+
+export function generateCalendar(
+  year: number,
+  month: number,
+  locale: locales  = locales.ENUS,
+  format: formatOptions = defaultFormatOptions,
+) {
   // we dont need to work with local time since we are not interested in the time
   // we only want the days, dates and months, we will never return time
   // the +1 is to make sure we get the last day of the month because js rolls back to the previous month
-  const daysInMonth = new Date(Date.UTC(year, month + 1, 0)).getUTCDate()
-  const firstDay = new Date(Date.UTC(year, month, 1)).getDay()
+  const daysInMonth = new Date(Date.UTC(year, month + 1, 0)).getUTCDate();
+  const _firstDay = new Date(Date.UTC(year, month, 1)).getDay();
   // using that logic we can get the last day of the previous month aka number of days in the previous month
-  const lastDayPreviousMonth = new Date(Date.UTC(year, month, 0)).getDate()
-  const lastDayIndex = new Date(Date.UTC(year, month - 1, lastDayPreviousMonth)).getDay()
-  const firstDayNextMonth = new Date(Date.UTC(year, month + 1, 1)).getDate()
-  const firstDayIndex = new Date(Date.UTC(year, month + 1, 1)).getDay()
+  const lastDayPreviousMonth = new Date(Date.UTC(year, month, 0)).getDate();
+  const lastDayIndex = new Date(Date.UTC(year, month - 1, lastDayPreviousMonth))
+    .getDay();
+  const _firstDayNextMonth = new Date(Date.UTC(year, month + 1, 1)).getDate();
+  const firstDayIndex = new Date(Date.UTC(year, month + 1, 1)).getDay();
 
-  console.log(lastDayPreviousMonth, lastDayIndex, firstDayNextMonth, firstDayIndex)
+  const calendarDates = new Array<string>();
+  const calendarWeeks = new Array<Array<string>>();
 
-  const calendarDates = new Array<Date>()
-  const calendarWeeks = new Array<Array<Date>>()
-  
   for (let j = lastDayIndex; j >= 0; j--) {
-    calendarDates.push(new Date(Date.UTC(year, month - 1, lastDayPreviousMonth - j)))
+    const date = new Date(Date.UTC(year, month - 1, lastDayPreviousMonth - j))
+      .toLocaleString(locale, format);
+    calendarDates.push(date);
   }
 
   for (let i = 1; i <= daysInMonth; i++) {
-    calendarDates.push(new Date(Date.UTC(year, month, i)))
+    const date = new Date(Date.UTC(year, month, i)).toLocaleString(
+      locale,
+      format,
+    );
+    calendarDates.push(date);
+  }
+
+  for (let k = 0; k <= 6 - firstDayIndex; k++) {
+    const date = new Date(Date.UTC(year, month + 1, k + 1)).toLocaleString(
+      locale,
+      format,
+    );
+    calendarDates.push(date);
+  }
+
+  const numberOfWeeks = Math.ceil(calendarDates.length / 7);
+  for (let i = 0; i < numberOfWeeks; i++) {
+    calendarWeeks.push(calendarDates.slice(i * 7, i * 7 + 7));
   }
   
-  for (let k = 0; k <= 6 - firstDayIndex; k++) {
-    calendarDates.push(new Date(Date.UTC(year, month + 1, k + 1)))
-  }
+  const monthName = returnMonthName(month, locale);
 
-  console.log(calendarDates)
-  const numberOfWeeks = Math.ceil(calendarDates.length / 7)
-  for (let i = 0; i < numberOfWeeks; i++) {
-    calendarWeeks.push(calendarDates.slice(i * 7, i * 7 + 7))
-  }
+  console.log(calendarWeeks, calendarDates);
+  console.log(monthName);
 
   return {
+    month: monthName,
     calendarDates,
-    calendarWeeks
-  }
+    calendarWeeks,
+  };
 }
 
-generateCalendar(2025, 2, 'fr-FR')
+function returnMonthName(month: number, locale: locales) {
+  return new Date(Date.UTC(2025, month, 1)).toLocaleString(locale, {
+    month: "long",
+  });
+}
+
+generateCalendar(2025, 3, locales.FRFR);
